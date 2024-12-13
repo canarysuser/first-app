1a7e266cf1f7f084aa65afffcb4feeb68c3d7cd9
Sudhir <sudhir.nayak.b@gmail.com>
2024-12-13 12:51:36 +0530

    Axios And Fetch API calls

---
+++ src/products/ProductApiService.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+const URL = "http://localhost:5000/products";
+
+const productList = [];
+const axiosInstance = axios.create({
+    baseURL:'http://localhost:5000',
+    timeout: 1000, 
+    headers: {'Content-Type':'application/json'},
+    maxBodyLength: 1000,
+    maxContentLength:65536
+})
+export async function getAllProducts() {
+    //var response = await axiosInstance.get('products');
+    var response = await axios({method: 'GET', url:URL});
+    return response.data;
+}
+
+export async function getProductDetails(productId) {
+    let pURL = `${URL}?productId=${productId}`
+    var response = await fetch(pURL);
+    var result = await response.json();
+    return result[0];
+}
+//UPdate or inSERT 
+export async function upsert(model) {
+    let method = 'POST'; //for INSERT operation
+    let pURL = `${URL}`;
+    let headers = {
+        'Content-Type': 'application/json'
+    };
+    var item = await getProductDetails(model.productId);
+    if (item) {
+        method = "PUT"; //For the UPDATE operation
+        pURL = `${URL}/${model.id}`
+    }
+    var body = JSON.stringify(model);
+    try {
+        var response = await fetch(pURL, {
+            method: method,
+            body: body,
+            headers: headers
+        });
+        var result = await response.json();
+        return result;
+        // return;
+    } catch (e) {
+        console.log(e);
+    }
+}
+export async function remove(productId) {
+    var item = await getProductDetails(productId);
+    if (item) {
+        let pURL = `${URL}/${item.id}`
+        var response = await fetch(pURL, {
+            method: 'DELETE'
+        });
+        var result = await response.json();
+        return result;
+    }
+}
--- src/products/productHome.jsx
+++ src/products/productHome.jsx
@@ -1,38 +1,44 @@
 import React, { useEffect, useState } from 'react'
 import Product from './product';
-import * as service from './productService';
+//import * as service from './productService';
+import * as service from './ProductApiService';
 import ProductList from './productList';
 import ProductDetails from './productDetails';
+
 
 function ProductHome() {
     const [productList, setProductList] = useState([]);
     const [selectedProduct, setSelectedProduct] = useState(new Product(0, '', 0, 0, false));
 
-    useEffect(() => {
-        if (!productList.length || productList.length < 1) {
-            var list = service.getAllProducts();
-            setProductList(list);
-        }
-        //console.log("useEffect called.")
-    },[selectedProduct])
 
-    var onSelectItem= (id)=> {
-       // alert('Selected Product Id: ' + id);
-       var item = service.getProductDetails(id); 
-       setSelectedProduct(item);
+    useEffect(() => {
+        async function loadProducts() {
+            if (!productList.length || productList.length < 1) {
+                var list = await service.getAllProducts();
+                setProductList(list);
+            }
+        }
+        loadProducts();
+        //console.log("useEffect called.")
+    }, [selectedProduct])
+
+    var onSelectItem = async (id) => {
+        // alert('Selected Product Id: ' + id);
+        var item = await service.getProductDetails(id);
+        setSelectedProduct(item);
     }
-    var onModelChanged = (model) => {
-        service.upsert(model); 
-        var list = service.getAllProducts(); 
+    var onModelChanged = async (model) => {
+        await service.upsert(model);
+        var list = await service.getAllProducts();
         setProductList([...list]);
     }
-    var onRemoveItem = (id)=>{
-        service.remove(id); 
-        var list = service.getAllProducts(); 
+    var onRemoveItem = async (id) => {
+        await service.remove(id);
+        var list = await service.getAllProducts();
         setProductList([...list]);
     }
-    var onAddNewItem = ()=>{
-        let obj=new Product(0, '', 0, 0, false);
+    var onAddNewItem = () => {
+        let obj = new Product(0, '', 0, 0, false);
         setSelectedProduct(obj)
     }
     return (
@@ -46,17 +52,17 @@ function ProductHome() {
                             <p>No records to show</p>
                     }
                     <ProductList productList={productList}
-                                 selectItem={onSelectItem}
-                                 removeItem={onRemoveItem}
-                                 addNewItem={onAddNewItem}/>
+                        selectItem={onSelectItem}
+                        removeItem={onRemoveItem}
+                        addNewItem={onAddNewItem} />
                 </div>
                 <div className='col-6'>
-                 
+
                     {/* Selected Product: 
                     <br/>
                     {JSON.stringify(selectedProduct)}
                     <br/> */}
-                    <ProductDetails model={selectedProduct} onSubmit={onModelChanged}/>
+                    <ProductDetails model={selectedProduct} onSubmit={onModelChanged} />
                 </div>
             </div>
         </div>
